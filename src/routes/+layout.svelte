df454eb970bdb1336ec3251eff7b3c08c5a25461
JiaJasonLiu <jialiu505@gmail.com>
2025-07-28 20:51:08 +0100

    Adding the icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,12 @@
 <header class="fixed top-0 left-0 right-0 bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm z-50 border-b border-gray-200 dark:border-gray-700">
   <nav in:slide={{axis:'y', duration:800, delay:400}} class="max-w-7xl mx-auto p-4">
     <div class="flex items-center justify-between">
-      <a href="/" onclick={() => showNav = false} class="text-2xl font-mono text-gray-900 dark:text-white hover:text-purple-800">JiaJason</a>
+      <a href="/" onclick={() => showNav = false} class="flex text-2xl font-mono text-gray-900 dark:text-white hover:text-purple-800">
+      <img src="/logo.png" alt="JiaJason's Pic" class="right-0 mt-1 rounded-full size-6 shadow-2xl">
+      <span class="-ml-1">
+        ason Liu
+      </span>
+      </a>
       <div class="flex space-x-8">
         <a href="/project" onclick={() => showNav = false} class="text-xl font-mono dark:text-gray-300 hover:text-purple-800 dark:hover:text-blue-400 transition-colors">Projects</a>
         <a href="/blog" onclick={() => showNav = false} class="text-xl font-mono dark:text-gray-300 hover:text-purple-800 dark:hover:text-blue-400 transition-colors">Blogs</a>
