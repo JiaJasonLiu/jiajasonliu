2c85881da5ebb3a6913537c142dbba9163d8d18c
JiaJasonLiu <jialiu505@gmail.com>
2025-07-28 11:02:46 +0100

    making the website transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,37 @@
 <script>
-  let { children } = $props();
   import "../app.css";
+  import { slide, fade } from 'svelte/transition';
+
+  let { children } = $props();
+  let showNav = $state(false)
+
+  $effect(() => {
+    if (!showNav){
+      showNav = true;
+    }
+	});
 
 </script>
-<nav>
+
+{#if showNav}
 <header class="fixed top-0 left-0 right-0 bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm z-50 border-b border-gray-200 dark:border-gray-700">
-  <nav class="max-w-7xl mx-auto p-4">
+  <nav in:slide={{axis:'y', duration:800, delay:400}} class="max-w-7xl mx-auto p-4">
     <div class="flex items-center justify-between">
-      <a href="/" class="text-2xl font-mono text-gray-900 dark:text-white hover:text-purple-800">JiaJason</a>
+      <a href="/" onclick={() => showNav = false} class="text-2xl font-mono text-gray-900 dark:text-white hover:text-purple-800">JiaJason</a>
       <div class="flex space-x-8">
-        <a href="/project" class="text-xl font-mono dark:text-gray-300 hover:text-purple-800 dark:hover:text-blue-400 transition-colors">Projects</a>
-        <a href="/blog" class="text-xl font-mono dark:text-gray-300 hover:text-purple-800 dark:hover:text-blue-400 transition-colors">Blogs</a>
+        <a href="/project" onclick={() => showNav = false} class="text-xl font-mono dark:text-gray-300 hover:text-purple-800 dark:hover:text-blue-400 transition-colors">Projects</a>
+        <a href="/blog" onclick={() => showNav = false} class="text-xl font-mono dark:text-gray-300 hover:text-purple-800 dark:hover:text-blue-400 transition-colors">Blogs</a>
       </div>
     </div>
   </nav>
 </header>
-</nav>
+{/if}
 
-<main class="mt-24">
+{#if showNav}
+<main in:fade={{duration:1500, delay:600}} class="mt-24">
   <div class="container mx-auto px-4">
   {@render children()}
   </div>
 </main>
+{/if}
 
