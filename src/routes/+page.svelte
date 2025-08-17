56c946fa342406ba9c8df2a36a15106b53b93fa5
JiaJasonLiu <jialiu505@gmail.com>
2025-08-17 15:10:47 +0200

    making the transistion and the lets contect button both work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,16 +11,25 @@
    * @type {HTMLDivElement}
    */
   let experienceNode;
+  /**
+   * @type {HTMLDivElement}
+   */
+  let contactNode;
   let contactSection = false;
   let hasAnimated = false;
+  let hasAnimatedContacts = false;
    /**
    * @param {{ preventDefault: () => void; currentTarget: { getAttribute: (arg0: string) => any; }; }} event
    */
   function scrollToSection(event) {
     event.preventDefault();
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     contactSection = true;
-
+    hasAnimated = true;
+    hasAnimatedContacts = true;
+    setTimeout(() => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+      
+    }, 200);
   }
 
 </script>
@@ -63,7 +72,7 @@
       <div >
         <IntersectionObserver once element={node} let:intersecting>
           <div bind:this={node} class="space-y-8">
-            {#if intersecting}
+            <!-- {#if intersecting} -->
               <div in:fade={{duration:500, delay:800}} class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg">
                 <div class="flex items-center dark:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -98,7 +107,7 @@
                 Scalable APIs, databases, and core logic using Node.js, Express, and PostgreSQL.
               </p>
             </div>
-            {/if}
+            <!-- {/if} -->
           </div>
         </IntersectionObserver> 
       </div>
@@ -107,10 +116,10 @@
 
   <!-- Experience Section -->
   <section id="experience">
-    <IntersectionObserver element={experienceNode} let:intersecting>
-          <div bind:this={experienceNode} class="space-y-8">
-            {#if intersecting || hasAnimated }
-              <div in:fade={{duration:500, delay:400}} class="max-w-7xl mx-auto"  on:introend={() => hasAnimated = true}>      
+  <IntersectionObserver element={experienceNode} let:intersecting>
+        <div bind:this={experienceNode} class="space-y-8 block">
+          {#if intersecting || hasAnimated }
+              <div in:fade={{duration: 500, delay:400}} class="max-w-7xl mx-auto"  on:introend={() => hasAnimated = true}>      
                 <h2 class="text-3xl font-bold mb-4 text-start text-gray-900 dark:text-white">Experience</h2>
                 <div class="space-y-8">
                   <!-- Experience items will go here -->
@@ -134,35 +143,41 @@
                 </div>
               </div>
               {/if}
-          </div>
-    </IntersectionObserver>
-  </section>
+            </div>
+          </IntersectionObserver>
+        </section>
 
   <!-- Contact Section -->
   <section id="contact" class="py-20 pb-50 px-4">
-    <div class="max-w-4xl mx-auto text-center">
-      <h2 class="text-3xl font-bold mb-8 text-gray-900 dark:text-white">Get in Touch</h2>
-      <p class="text-xl text-gray-600 dark:text-gray-300 mb-8">I'm always open to new opportunities and collaborations.</p>
-      <div class="flex justify-center space-x-6 {contactSection ? 'animate-bounce' : ''} ">
-        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jial7150@gmail.com" target="_blank" class="flex items-center gap-1 text-lg text-gray-600 dark:text-gray-300 hover:text-purple-800" aria-label="Send me an email">
-          <svg class="w-6 h-6"  fill="currentColor" viewBox="0 0 50 50">
-            <path d="M 43.753906 6.4023438 C 42.53621 6.3489969 41.294792 6.712898 40.271484 7.46875 L 37.525391 9.4960938 L 25 18.755859 L 12.591797 9.5839844 A 1.0001 1.0001 0 0 0 11.949219 9.3007812 L 12.199219 9.3007812 L 9.734375 7.4765625 C 8.7104042 6.7188363 7.4671493 6.3528895 6.2480469 6.40625 C 5.0289444 6.4596105 3.8349462 6.9314667 2.9082031 7.8457031 C 1.7309454 9.0063798 1 10.629831 1 12.410156 L 1 15.84375 A 1.0001 1.0001 0 0 0 1 16.138672 L 1 39.5 C 1 41.421188 2.5788117 43 4.5 43 L 12 43 A 1.0001 1.0001 0 0 0 13 42 L 13 25.373047 L 24.40625 33.804688 A 1.0001 1.0001 0 0 0 25.59375 33.804688 L 37 25.373047 L 37 42 A 1.0001 1.0001 0 0 0 38 43 L 45.5 43 C 47.421188 43 49 41.421188 49 39.5 L 49 16.119141 A 1.0001 1.0001 0 0 0 49 15.859375 L 49 12.410156 C 49 10.6517 48.290455 9.0357821 47.128906 7.8730469 C 47.095336 7.8394769 47.084086 7.83018 47.097656 7.84375 A 1.0001 1.0001 0 0 0 47.091797 7.8378906 C 46.165242 6.9256756 44.971603 6.4556905 43.753906 6.4023438 z M 43.644531 8.4003906 C 44.400835 8.4300436 45.134049 8.7168876 45.689453 9.2636719 C 45.708363 9.2823439 45.722171 9.2964424 45.712891 9.2871094 C 46.50934 10.084374 47 11.188613 47 12.410156 L 47 15.496094 L 39 21.408203 L 39 11 A 1.0001 1.0001 0 0 0 38.996094 10.898438 L 41.458984 9.078125 A 1.0001 1.0001 0 0 0 41.460938 9.078125 C 42.109578 8.598977 42.888228 8.3707375 43.644531 8.4003906 z M 6.3574219 8.40625 C 7.1145694 8.37661 7.8958927 8.6037105 8.5449219 9.0839844 L 11.003906 10.902344 A 1.0001 1.0001 0 0 0 11 11 L 11 21.408203 L 3 15.496094 L 3 12.410156 C 3 11.174482 3.5017577 10.068855 4.3125 9.2695312 C 4.8677569 8.7217677 5.6002743 8.4358895 6.3574219 8.40625 z M 37 12.371094 L 37 22.886719 L 25 31.755859 L 13 22.886719 L 13 12.373047 L 24.40625 20.804688 A 1.0001 1.0001 0 0 0 25.59375 20.804688 L 37 12.371094 z M 3 17.982422 L 11 23.896484 L 11 41 L 4.5 41 C 3.6591883 41 3 40.340812 3 39.5 L 3 17.982422 z M 47 17.982422 L 47 39.5 C 47 40.340812 46.340812 41 45.5 41 L 39 41 L 39 23.896484 L 47 17.982422 z"></path>
-          </svg>
-          Email
-        </a>
-        <a href="https://github.com/JiaJasonLiu" target="_blank" class="flex items-center gap-1 text-lg text-gray-600 dark:text-gray-300 hover:text-purple-800 {contactSection ? 'animate-bounce' : ''}" aria-label="Visit my GitHub profile">
-          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-          </svg>
-          GitHub
-        </a>
-        <a href="https://www.linkedin.com/in/jia-sheng-liu-9a8a23224/" target="_blank" class="flex items-center gap-1 text-lg text-gray-600 dark:text-gray-300 hover:text-purple-800" aria-label="Visit my LinkedIn profile">
-          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 30 30">
-              <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
-          </svg>
-          LinkedIn
-        </a>
-      </div>
-    </div>
-  </section>
+   <IntersectionObserver element={contactNode} let:intersecting>
+        <div bind:this={contactNode} class="space-y-8">
+          {#if intersecting || hasAnimatedContacts }
+              <div in:fade={{duration:500, delay:400}} class="max-w-4xl mx-auto text-center" on:introend={() => hasAnimatedContacts = true}>
+                <h2 class="text-3xl font-bold mb-8 text-gray-900 dark:text-white">Get in Touch</h2>
+                <p class="text-xl text-gray-600 dark:text-gray-300 mb-8">I'm always open to new opportunities and collaborations.</p>
+                <div class="flex justify-center space-x-6 {contactSection ? 'animate-bounce' : ''} ">
+                  <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jial7150@gmail.com" target="_blank" class="flex items-center gap-1 text-lg text-gray-600 dark:text-gray-300 hover:text-purple-800" aria-label="Send me an email">
+                    <svg class="w-6 h-6"  fill="currentColor" viewBox="0 0 50 50">
+                      <path d="M 43.753906 6.4023438 C 42.53621 6.3489969 41.294792 6.712898 40.271484 7.46875 L 37.525391 9.4960938 L 25 18.755859 L 12.591797 9.5839844 A 1.0001 1.0001 0 0 0 11.949219 9.3007812 L 12.199219 9.3007812 L 9.734375 7.4765625 C 8.7104042 6.7188363 7.4671493 6.3528895 6.2480469 6.40625 C 5.0289444 6.4596105 3.8349462 6.9314667 2.9082031 7.8457031 C 1.7309454 9.0063798 1 10.629831 1 12.410156 L 1 15.84375 A 1.0001 1.0001 0 0 0 1 16.138672 L 1 39.5 C 1 41.421188 2.5788117 43 4.5 43 L 12 43 A 1.0001 1.0001 0 0 0 13 42 L 13 25.373047 L 24.40625 33.804688 A 1.0001 1.0001 0 0 0 25.59375 33.804688 L 37 25.373047 L 37 42 A 1.0001 1.0001 0 0 0 38 43 L 45.5 43 C 47.421188 43 49 41.421188 49 39.5 L 49 16.119141 A 1.0001 1.0001 0 0 0 49 15.859375 L 49 12.410156 C 49 10.6517 48.290455 9.0357821 47.128906 7.8730469 C 47.095336 7.8394769 47.084086 7.83018 47.097656 7.84375 A 1.0001 1.0001 0 0 0 47.091797 7.8378906 C 46.165242 6.9256756 44.971603 6.4556905 43.753906 6.4023438 z M 43.644531 8.4003906 C 44.400835 8.4300436 45.134049 8.7168876 45.689453 9.2636719 C 45.708363 9.2823439 45.722171 9.2964424 45.712891 9.2871094 C 46.50934 10.084374 47 11.188613 47 12.410156 L 47 15.496094 L 39 21.408203 L 39 11 A 1.0001 1.0001 0 0 0 38.996094 10.898438 L 41.458984 9.078125 A 1.0001 1.0001 0 0 0 41.460938 9.078125 C 42.109578 8.598977 42.888228 8.3707375 43.644531 8.4003906 z M 6.3574219 8.40625 C 7.1145694 8.37661 7.8958927 8.6037105 8.5449219 9.0839844 L 11.003906 10.902344 A 1.0001 1.0001 0 0 0 11 11 L 11 21.408203 L 3 15.496094 L 3 12.410156 C 3 11.174482 3.5017577 10.068855 4.3125 9.2695312 C 4.8677569 8.7217677 5.6002743 8.4358895 6.3574219 8.40625 z M 37 12.371094 L 37 22.886719 L 25 31.755859 L 13 22.886719 L 13 12.373047 L 24.40625 20.804688 A 1.0001 1.0001 0 0 0 25.59375 20.804688 L 37 12.371094 z M 3 17.982422 L 11 23.896484 L 11 41 L 4.5 41 C 3.6591883 41 3 40.340812 3 39.5 L 3 17.982422 z M 47 17.982422 L 47 39.5 C 47 40.340812 46.340812 41 45.5 41 L 39 41 L 39 23.896484 L 47 17.982422 z"></path>
+                    </svg>
+                    Email
+                  </a>
+                  <a href="https://github.com/JiaJasonLiu" target="_blank" class="flex items-center gap-1 text-lg text-gray-600 dark:text-gray-300 hover:text-purple-800 {contactSection ? 'animate-bounce' : ''}" aria-label="Visit my GitHub profile">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                    </svg>
+                    GitHub
+                  </a>
+                  <a href="https://www.linkedin.com/in/jia-sheng-liu-9a8a23224/" target="_blank" class="flex items-center gap-1 text-lg text-gray-600 dark:text-gray-300 hover:text-purple-800" aria-label="Visit my LinkedIn profile">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 30 30">
+                        <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
+                    </svg>
+                    LinkedIn
+                  </a>
+                </div>
+              </div>
+              {/if}
+            </div>
+          </IntersectionObserver>
+        </section>
 </main>
