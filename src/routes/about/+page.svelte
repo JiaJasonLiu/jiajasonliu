6e4c032a7f6e612780f2ce13ee583dcd7e538a5c
jason <jialiu505@gmail.com>
2025-12-31 14:41:03 +0000

    adding about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+  <section id="hero" class="mb-32">
+    <div class="max-w-7xl mx-auto flex py-8 pt-24 justify-between gap-15 items-center">
+    <div class="flex flex-col items-start">
+      <h1 class="text-4xl font-bold py-8 mt-10 text-gray-900 dark:text-gray-300">Always have a Plan Z</h1>
+      <h2 class="mt-8 text-xl text-gray-500 dark:text-gray-400">
+        Have a dream to pursue is the most fulfilling aspect of life I can imagine. 
+        Hurdles with arise. 
+        Challenges will be faced.
+        Backup plans will be made.
+        Nonetheless, I will continue to strive to solve problems and achieve my goals in my own way by being a
+        <strong> Software Engineer.</strong>
+      </h2>
+      <p class="mt-6 text-base text-gray-500 dark:text-gray-400">
+        If you share the same passion for problem-solving, innovation, and outlook.
+      </p>
+      <button on:click={() => console.log("About Page")} aria-label="Let us Connect" class="mt-6 px-6 py-3 bg-white-600 dark:border-indigo-800 text-black dark:hover:border-black dark:text-gray-100 border font-semibold rounded-xl hover:text-white hover:bg-indigo-800 transition duration-300 shadow-2xl shadow-indigo-500/30">
+        Let's Connect
+      </button>
+    </div>
+    <img src="/jiajason.png" alt="JiaJason's Pic" class="md:inline hidden right-0 rounded-full max-h-115 shadow-2xl dark:opacity-75 shadow-indigo-500/30">
+     </div>
+  </section>
